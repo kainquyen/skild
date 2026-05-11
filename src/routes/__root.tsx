5841cc99040098a8cb095e8c804def874111cbbf
Nguyễn Phú Khải <nkhai648@gmail.com>
2026-05-11 09:18:00 +0700

    comment debug tanstack start

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,15 +1,12 @@
-import { TanStackDevtools } from "@tanstack/react-devtools";
 import type { QueryClient } from "@tanstack/react-query";
 import {
 	createRootRouteWithContext,
 	HeadContent,
 	Scripts,
 } from "@tanstack/react-router";
-import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 import Crosshair from "#/components/Crosshair";
 import Navbar from "#/components/Navbar";
 import { ClerkProvider } from '@clerk/tanstack-react-start'
-import TanStackQueryDevtools from "../integrations/tanstack-query/devtools";
 import appCss from "../styles.css?url";
 
 interface MyRouterContext {
@@ -67,18 +64,6 @@ function RootDocument({ children }: { children: React.ReactNode }) {
 								<div className="frame">{children}</div>
 							</main>
 						</div>
-						<TanStackDevtools
-							config={{
-								position: "bottom-right",
-							}}
-							plugins={[
-								{
-									name: "Tanstack Router",
-									render: <TanStackRouterDevtoolsPanel />,
-								},
-								TanStackQueryDevtools,
-							]}
-						/>
 					</ClerkProvider>
 				<Scripts />
 			</body>
